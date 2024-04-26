a5d1b417f369d8fe73a38a8e6e49b5dac651d938
lucas.ying@menloschool.org <lucas.ying@menloschool.org>
2024-04-26 09:56:56 -0700

    Added data and in progress on tutorial for program

--- identifier.py
+++ identifier.py
@@ -1,3 +1,4 @@
+# Lucas Ying Final Project
 # A program to identifier the type of sneaker from an image or video
 
 
@@ -21,4 +22,46 @@ def label_img(img):
     word_label = img.split('_')[0]
     if word_label == 'OFFWHITE': return [1,0] #one hot encoding
     elif word_label == 'YEEZY': return [0,1] #one hot encoding
+
+def create_train_data():
+    train_data = []
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        label = label_img(img)
+        path = os.path.join(TRAIN_DIR, img)
+        ############################################################
+        #    This part is different from sentdex's tutorial
+        # Chose to use PIL instead of cv2 for image pre-processing
+        ############################################################
+        
+        img = Image.open(path) #Read image syntax with PIL Library
+        img = img.convert('L') #Grayscale conversion with PIL library
+        img = img.resize((IMG_SIZE, IMG_SIZE), Image.ANTIALIAS) #Resizing image syntax with PIL Library
+        
+        ############################################################
+        
+        train_data.append([np.array(img), np.array(label)])
+    shuffle(train_data)
+    np.save('train_data.npy', train_data) #.npy extension = numpy file
+    return train_data
+
+def process_test_data():
+    test_data = []
+    for img in tqdm(os.listdir(TEST_DIR)):
+        path = os.path.join(TEST_DIR, img)
+        if "DS_Store" not in path:
+            img_num = img.split('_')[1] #images are formatted 'NIKE_2', 'ADIDAS_56'..
+        
+            #PIL LIBRARY instead of cv2
+            img = Image.open(path)
+            img = img.convert('L')
+            img = img.resize((IMG_SIZE, IMG_SIZE), Image.ANTIALIAS)
+        
+            test_data.append([np.array(img), img_num])
+    shuffle(test_data)
+    np.save('test_data.npy', test_data)
+    return test_data
+
+train_data = create_train_data()
+plt.imshow(train_data[43][0], cmap = 'gist_gray')
+print(train_data[43][1])
 
